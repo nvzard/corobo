0c3e729e3fd69a099815ebdfe9b86cd15b0ae7c3
Nitanshu Vashistha <nitanshu.vzard@gmail.com>
2019-03-03 16:52:24 +0530

    Fix Semaphore

--- .ci/semaphore.tests.sh
+++ .ci/semaphore.tests.sh
@@ -1,9 +1,13 @@
 set -e -x
 
-pip install coala-bears
+cp requirements.txt requirements.orig
+printf '%s\n%s\n%s\n%s\n' \
+    "git+https://github.com/coala/coala" \
+    "git+https://github.com/coala/coala-bears" \
+    "$(cat test-requirements.txt)" \
+    "$(cat requirements.txt)" \
+    > requirements.txt
 pip install -r requirements.txt
-pip install -r test-requirements.txt
-sed -i.bak '/bears = GitCommitBear/d' .coafile
 coala --ci -V
 python -m pytest
 codecov
